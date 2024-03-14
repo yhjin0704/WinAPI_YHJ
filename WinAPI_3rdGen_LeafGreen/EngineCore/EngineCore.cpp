c01f1b337c838043b7db48865f184035c2bab447
YHJ <yhjin07@naver.com>
2024-03-14 20:01:14 +0900

    엔진 업데이트

--- WinAPI_3rdGen_LeafGreen/EngineCore/EngineCore.cpp
+++ WinAPI_3rdGen_LeafGreen/EngineCore/EngineCore.cpp
@@ -74,7 +74,14 @@ void UEngineCore::CoreTick()
 
 		AllLevel.erase(DestroyLevelName[i]);
 
+		if (Level == CurLevel)
+		{
+			CurLevel = nullptr;
+		}
+
 		Level->End();
+
+		
 
 		if (nullptr != Level)
 		{
@@ -177,7 +184,8 @@ void UEngineCore::CoreInit(HINSTANCE _HINSTANCE)
 	}
 
 	UEngineWindow::Init(_HINSTANCE);
-	MainWindow.Open();
+	WindowOpen(WindowTitle, WindowIconPath);
+	MainWindow.Open(WindowTitle, WindowIconPath);
 
 	this->AllLevel;
 
